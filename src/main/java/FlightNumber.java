dffa5dc5fb5437aae3ef5af4bd57b004667a5bda
kaluzsikc <kaluzsikc@gmail.com>
2018-10-12 12:28:59 +0100

    Added FlightNumber enums

---
+++ src/main/java/FlightNumber.java
@@ -0,0 +1,20 @@
+public enum FlightNumber {
+
+    A123("LONDON"),
+    B123("SEICHELLES"),
+    C123("BARCELONA"),
+    D123("DUBAI"),
+    E123("LISBON");
+
+    private final String destination;
+
+    FlightNumber(String destination){
+        this.destination = destination;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+
+}
