8dcdd8cdfedf01bf3af38b98ceb8a13df19d8d1e
kaluzsikc <kaluzsikc@gmail.com>
2018-10-12 12:22:16 +0100

    Plane class and getters created

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Plane {
+
+    private Airline airline;
+    private PlaneType planeType;
+    private ArrayList<Passenger> passengers;
+
+    public Plane (Airline airline, PlaneType planeType){
+
+        this.airline = airline;
+        this.planeType = planeType;
+        this.passengers = new ArrayList<>();
+    }
+
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public PlaneType getType() {
+        return planeType;
+    }
+
+    public int getNumberOfPassengers() {
+        return passengers.size();
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(Airline.LUFTHANSA, PlaneType.AIRBUS319);
+    }
+
+    @Test
+    public void hasAirline() {
+        assertEquals(Airline.LUFTHANSA, plane.getAirline());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals(PlaneType.AIRBUS319, plane.getType());
+    }
+
+    @Test
+    public void hasNoPassengers() {
+        assertEquals(0, plane.getNumberOfPassengers());
+    }
 }
