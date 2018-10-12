c0c633cf818705e3d8864a48829d3ecec97564aa
kaluzsikc <kaluzsikc@gmail.com>
2018-10-12 12:51:59 +0100

    Added Flight class and set getters

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,23 @@
 public class Flight {
+
+    private PlaneType planeType;
+    private FlightNumber flightNumber;
+
+    public Flight(PlaneType planeType, FlightNumber flightNumber){
+
+        this.planeType = planeType;
+        this.flightNumber = flightNumber;
+    }
+
+    public PlaneType getPlaneType() {
+        return planeType;
+    }
+
+    public FlightNumber getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getFlightDestination() {
+        return flightNumber.getDestination();
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+
+    @Before
+    public void before() {
+        flight = new Flight(PlaneType.AIRBUS319, FlightNumber.A123);
+    }
+
+    @Test
+    public void hasPlane() {
+        assertEquals(PlaneType.AIRBUS319, flight.getPlaneType());
+    }
+
+    @Test
+    public void hasFlightNumber() {
+        assertEquals(FlightNumber.A123, flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination() {
+        assertEquals("LONDON", flight.getFlightDestination());
+    }
 }
